6cea4708c32be896222f8b4c153a126fed933744
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2025-12-24 17:37:58 +1100

    aaaaa

--- src/rdrnaseq/stages.py
+++ src/rdrnaseq/stages.py
@@ -239,7 +239,7 @@ class Outrider(stage.CohortStage):
     Perform outlier gene expression analysis with Outrider.
     """
 
-    def expected_outputs(self, cohort: targets.Cohort) ->  dict[str, Path]:
+    def expected_outputs(self, cohort: targets.Cohort) -> dict[str, Path]:
         """
         Generate outrider outputs.
         """
