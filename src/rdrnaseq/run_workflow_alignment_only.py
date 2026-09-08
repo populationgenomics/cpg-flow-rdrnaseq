eb85b0cc157b30ce0c8d3ea8e250d07aa7199e2b
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-09-08 11:20:32 +1000

    adding this to the alignment stage

--- src/rdrnaseq/run_workflow_alignment_only.py
+++ src/rdrnaseq/run_workflow_alignment_only.py
@@ -13,7 +13,14 @@ from argparse import ArgumentParser
 
 from cpg_flow.workflow import run_workflow
 
-from rdrnaseq.stages import Count, Somalier, TrimAlignRNA
+from rdrnaseq.stages import (
+    Count,
+    FastpMultiQC,
+    FastpQC,
+    SamtoolsStats,
+    Somalier,
+    TrimAlignRNA,
+)
 
 
 def cli_main() -> None:
@@ -30,7 +37,17 @@ def cli_main() -> None:
 
     # Otherwise all configuration should be done by providing all relevant configs to analysis-runner
     # https://github.com/populationgenomics/team-docs/blob/main/cpg_utils_config.md#config-in-analysis-runner-jobs
-    stages = [TrimAlignRNA, Count, Somalier]
+    stages = [
+        TrimAlignRNA,
+        Count,
+        Somalier,
+        FastpQC,
+        FastpMultiQC,
+        TrimAlignRNA,
+        Count,
+        Somalier,
+        SamtoolsStats,
+    ]
     run_workflow(name='rdrnaseq', stages=stages, dry_run=args.dry_run)
 
 
